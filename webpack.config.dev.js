b842f11cf10f0b3ab1c12cd7f9ffe486bb610fdc
jesusRamon <jesus.ramon2192@gmail.com>
2023-01-25 12:16:34 -0600

    Clase 20 Webpack Bundle Analyzer

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer');
 
 module.exports = {
   // Entry nos permite decir el punto de entrada de nuestra aplicación
@@ -108,6 +109,7 @@ module.exports = {
       ]
     }),
     new Dotenv(),
+    new BundleAnalyzerPlugin(),
   ],
   devServer: {
     static: {
